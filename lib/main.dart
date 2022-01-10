485cbcadf6d489cef925cc2e5e977a9879ec500b
al-hp <al2718x@gmail.com>
2022-01-10 20:12:51 +0300

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // @dart=2.9
 
 import 'package:barcode_scan/barcode_scan.dart';
-import 'package:barcode_scan/platform_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
