b2076f8927d5f851de8db86b9a4a503a3a594089
al-hp <al2718x@gmail.com>
2023-10-01 00:28:27 +0300

    confirm delete, refactor

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final List<String> _items = [];
+  int _toDelete = -1;
 
   _scan() async {
     var result = await BarcodeScanner.scan();
@@ -39,96 +40,105 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    _items.clear();
     _items.addAll(Preferences.getList('urls'));
     _scan();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QR Tool',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      builder: (context, child) => Scaffold(
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          color: Colors.white,
-          padding: EdgeInsets.fromLTRB(12, MediaQuery.of(context).padding.top + 12, 12, 12),
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: const Color.fromARGB(127, 0xff, 0xaa, 0x80),
-                child: TextButton(
-                  onPressed: () async {
-                    await _scan();
-                  },
-                  child: Row(children: const <Widget>[
-                    Spacer(),
-                    Icon(Icons.camera, size: 32),
-                    Text(' QR', style: TextStyle(fontSize: 24)),
-                    Spacer(),
-                  ]),
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'QR Tool',
+        builder: (context, child) {
+          double textWidth = MediaQuery.of(context).size.width - 90;
+          return SafeArea(
+            child: Scaffold(
+              body: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: double.infinity,
+                      color: const Color.fromARGB(127, 0xff, 0xaa, 0x80),
+                      child: TextButton(
+                        onPressed: () async {
+                          await _scan();
+                        },
+                        child: const Icon(Icons.qr_code, size: 48),
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _items.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            padding: const EdgeInsets.only(bottom: 12),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                InkWell(
+                                  onTap: () async {
+                                    if (index == _toDelete) {
+                                      _toDelete = -1;
+                                      _items.removeAt(index);
+                                      Preferences.setList('urls', _items);
+                                    } else {
+                                      _toDelete = index;
+                                      Future.delayed(const Duration(seconds: 3), () {
+                                        _toDelete = -1;
+                                        setState(() {});
+                                      });
+                                    }
+                                    setState(() {});
+                                  },
+                                  child: Container(
+                                    padding: const EdgeInsets.only(right: 10),
+                                    child: Icon(
+                                      Icons.auto_delete_outlined,
+                                      color: (index == _toDelete) ? Colors.red : null,
+                                    ),
+                                  ),
+                                ),
+                                InkWell(
+                                  onTap: () async {
+                                    await Clipboard.setData(ClipboardData(text: _items[index]));
+                                  },
+                                  child: Container(
+                                    padding: const EdgeInsets.only(right: 8),
+                                    child: const Icon(Icons.copy),
+                                  ),
+                                ),
+                                InkWell(
+                                  onTap: () async {
+                                    await launchUrlString(_items[index], mode: LaunchMode.externalApplication);
+                                  },
+                                  child: SizedBox(
+                                    width: textWidth,
+                                    child: Text(
+                                      _items[index],
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.blue[900],
+                                        decoration: TextDecoration.underline,
+                                        decorationColor: Colors.blue[900],
+                                        decorationStyle: TextDecorationStyle.solid,
+                                      ),
+                                      overflow: TextOverflow.fade,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Expanded(
-                child: ListView.builder(
-                    itemCount: _items.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        padding: const EdgeInsets.only(bottom: 12),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            InkWell(
-                              onTap: () async {
-                                await Clipboard.setData(ClipboardData(text: _items[index]));
-                              },
-                              child: Container(
-                                padding: const EdgeInsets.only(right: 8),
-                                child: const Icon(Icons.content_copy),
-                              ),
-                            ),
-                            InkWell(
-                              onTap: () async {
-                                setState(() {
-                                  _items.removeAt(index);
-                                  Preferences.setList('urls', _items);
-                                });
-                              },
-                              child: Container(
-                                padding: const EdgeInsets.only(right: 10),
-                                child: const Icon(Icons.delete_outline),
-                              ),
-                            ),
-                            InkWell(
-                              onTap: () async {
-                                await launchUrlString(_items[index], mode: LaunchMode.externalApplication);
-                              },
-                              child: SizedBox(
-                                width: MediaQuery.of(context).size.width - 90,
-                                child: Text(
-                                  _items[index],
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.blue[900],
-                                    decoration: TextDecoration.underline,
-                                    decorationColor: Colors.blue[900],
-                                    decorationStyle: TextDecorationStyle.solid,
-                                  ),
-                                  overflow: TextOverflow.fade,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+            ),
+          );
+        },
+      );
 }
